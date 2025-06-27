3b5b7b0b9b87b8b601b216c60e087803549f8a30
gaganpratap2 <gaganpratapsingh61@gmail.com>
2025-06-28 00:53:11 +0530

    Implement first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger , SplitText } from 'gsap/all'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 gsap.registerPlugin(ScrollTrigger , SplitText)
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-4xl text-indigo-300'>Hello</h1>
-    </div>
+   <main>
+    <NavBar />
+    <Hero />
+
+    <div className="h-dvh bg-black"></div>
+   </main>
   )
 }
 
